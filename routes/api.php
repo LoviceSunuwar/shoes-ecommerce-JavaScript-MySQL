551ee96a2e9f18cf6a17c9911aae86fbde348499
Lovice Sunuwar <lovicesunuwar@gmail.com>
2021-03-18 13:10:44 +0545

    ecomm api

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('categories', 'Api\CategoryController@index');
+Route::get('products', 'Api\ProductController@index');
+Route::get('sliders', 'Api\SliderController@index');
+Route::get('orders', 'Api\orderController@index');
